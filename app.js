b268a0b4a493ac4b188846b93cb97017e72b8090
Rohit-Sarkar55 <71070509+Rohit-Sarkar55@users.noreply.github.com>
2022-05-08 19:05:17 +0530

    read more button added usinf jquery that loads up the rest content on the same page

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.static("public"));
 
 const posts = [];
 const posttitle = ""  , postContent = "";
+var _ = require('lodash');
 
 app.get("/" , function(req ,res){
   res.render("home" , { content:homeStartingContent , Blogs : posts } );
@@ -32,7 +33,6 @@ app.get("/contact", function(req ,res){
   res.render("contact" , { content : contactContent});
   
 });
-
 
 app.get("/compose" , function(req , res ){
   
@@ -60,9 +60,9 @@ app.post("/compose" , function(req , res){
   
 // });
 
-// app.get("/post" , function(req ,res){
-//   res.render("post", { x:posttitle, y:postContent } );
-// });
+app.get("/post" , function(req ,res){
+  res.render("post", { x:posttitle, y:postContent } );
+});
 app.get("/posts/:postName" , function(req , res ){
   const userUrl = _.lowerCase(req.params.postName);
 
@@ -77,8 +77,23 @@ app.get("/posts/:postName" , function(req , res ){
   })
   
 });
-
-
+// $(document).ready(function(){
+// 	var maxLength = 30;
+// 	$(".show-read-more").each(function(){
+// 		var myStr = $(this).text();
+// 		if($.trim(myStr).length > maxLength){
+// 			var newStr = myStr.substring(0, maxLength);
+// 			var removedStr = myStr.substring(maxLength, $.trim(myStr).length);
+// 			$(this).empty().html(newStr);
+// 			$(this).append(' <a href="javascript:void(0);" class="read-more">read more...</a>');
+// 			$(this).append('<span class="more-text">' + removedStr + '</span>');
+// 		}
+// 	});
+// 	$(".read-more").click(function(){
+// 		$(this).siblings(".more-text").contents().unwrap();
+// 		$(this).remove();
+// 	});
+// });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
